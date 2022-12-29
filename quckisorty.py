34b6ae39bc4695b4294a66b137fabdf5ace60e07
Amarnath <motorolaonefusion@gmail.com>
2022-12-29 22:33:47 +0530

    quick sort

--- quckisorty.py
+++ quckisorty.py
@@ -1,4 +1,4 @@
-def partion(list , low , high):
+def partition(list , low , high):
     i = low - 1
     pivot = list[high]
 
@@ -8,3 +8,15 @@ def partion(list , low , high):
             list[i] ,list[j] = list[j] ,list[i]
     list[i+1] , list[high] = list[high] , list[i+1]
     return (i+1)
+
+
+
+def quickSort(lists, low , high):
+    if low <  high:
+        pi = partition(lists, low , high)
+        quickSort(lists, low, pi-1)
+        quickSort(lists , pi+1 , high)
+
+lists= [2,1,7,6,5,3,4,9,8]
+quickSort(lists, 0 ,len(lists)-1 )
+print(lists)
